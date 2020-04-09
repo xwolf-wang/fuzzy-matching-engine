ad37bdb6e6d53d2ed5bc13f997214f7bad36e8a7
ming <wang_ming@iclould.com>
2020-04-09 23:04:38 +0800

    add Pattern and Matcher test cases

--- src/test/java/com/xwolf/os/TestRegex.java
+++ src/test/java/com/xwolf/os/TestRegex.java
@@ -1,18 +1,22 @@
 package com.xwolf.os;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TestRegex {
     public static void main(String[] args) {
         //Pattern(正则表达式编译)、Matcher（匹配）
-        String str ="INSERT INTO dept (deptno,dname,loc) VALUES (#{deptno},#{dname},#{loc})";
+        String str ="<fintrade><interfaceId>interfaceid-23r3--dfa</interfaceId><version>22</version></fintrade>";
         //要求获取#{}中的所有内容
-        String regex ="#\\{\\w+\\}";
+        String regex ="<interfaceId>(.*?)</interfaceId>";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
         while (matcher.find()){
-            System.out.println(matcher.group(0).replaceAll("#|\\{|\\}",""));
+            System.out.println(matcher.group(1));
+//            System.out.println(matcher.group().replaceAll("<interfaceId>","").replaceAll("</interfaceId>",""));
+//            System.out.println(matcher.group(0).replaceAll("#|\\{|\\}",""));
         }
 
     }
